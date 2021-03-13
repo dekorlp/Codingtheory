94c2caf72df73550b115bd5a53fc719d5932cfbc
Dennis Köhler <denniskoehler92@gmail.com>
2021-03-13 23:46:33 +0100

    Beispiele hinzugefügt

--- Praktikum2/Praktikum2/main.cpp
+++ Praktikum2/Praktikum2/main.cpp
@@ -4,7 +4,21 @@ int main()
 {
 	PolynomManager man;
 	//man.CreateTable(8);
-	Polynom result = man.CalculateInverse(Polynom({ PolynomPart(1,2), PolynomPart(1,0)}));
+	// Erstes Beispiel:
+	//Polynom result = man.CalculateInverse(Polynom({ PolynomPart(1,5), PolynomPart(1,4), PolynomPart(1,3), PolynomPart(1,2), PolynomPart(1,1), PolynomPart(1,0) }));
+
+	// Zweites Beispiel:
+	//Polynom result = man.CalculateInverse(Polynom({ PolynomPart(1,2), PolynomPart(1,0) }));
+	
+	// Drittes Beispiel:
+	//Polynom result = man.CalculateInverse(Polynom({ PolynomPart(1,1), PolynomPart(1,0) }));
+
+	// Viertes Beispiel:
+	//Polynom result = man.CalculateInverse(Polynom({ PolynomPart(1,2), PolynomPart(1,1) }));
+
+	// Fünftes Beispiel:
+	Polynom result = man.CalculateInverse(Polynom({ PolynomPart(1,2)}));
+
 	int test = 0;
 
 	return 0;
